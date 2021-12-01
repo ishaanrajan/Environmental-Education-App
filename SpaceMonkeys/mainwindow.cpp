30df352a06c916ba72634bba25e3c13a07ceb638
audreymiller12 <u1197148@utah.edu>
2021-12-01 11:36:46 -0700

    changed the drag and drop to be in the UI

--- SpaceMonkeys/mainwindow.cpp
+++ SpaceMonkeys/mainwindow.cpp
@@ -10,61 +10,29 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QWidget* dragWidget = new QWidget(this);
-    dragWidget->setStyleSheet("background-color: #ECF0F1");
-    setCentralWidget(dragWidget);
 
-    QVBoxLayout* dragMainLayout = new QVBoxLayout();
-    dragWidget->setLayout(dragMainLayout);
+    ui->dirtyEnergyListView->setDragEnabled(true);
+    ui->dirtyEnergyListView->setAcceptDrops(true);
+    ui->dirtyEnergyListView->setDefaultDropAction(Qt::MoveAction);
 
-    QLabel* title = new QLabel("Build Your Eco City", this);
-    dragMainLayout->addWidget(title);
-    title->setAlignment(Qt::AlignCenter);
-    title->setStyleSheet("font-size: 30pt; margin: 10%;");
+    ui->cleanEnergyListView->setDragEnabled(true);
+    ui->cleanEnergyListView->setAcceptDrops(true);
+    ui->cleanEnergyListView->setDefaultDropAction(Qt::MoveAction);
 
-    QHBoxLayout* categories = new QHBoxLayout();
-    dragMainLayout->addLayout(categories);
+    ui->cleanEnergyListView->setModel(new QStringListModel());
+    ui->dirtyEnergyListView->setModel(new QStringListModel());
 
-    //This is where "category sections" are added to the main window
-    QLabel* category1Label = new QLabel("Dirty Energy", this);
-    category1Label->setStyleSheet("font-size: 15pt;");
-    categories->addWidget(category1Label);
+    ui->dirtyEnergyListView->setStyleSheet
+                ("QListView { font-size: 20pt; font-weight: bold; }"
+                 "QListView::item { background-color: #E74C3C; padding: 10%;"
+                 "border: 1pt solid #C0392B; }"
+                 "QListView::item::hover {background-color: #C0392B }");
 
-    QLabel* category2Label = new QLabel("Clean Energy", this);
-    category2Label->setStyleSheet("font-size: 15pt;");
-    categories->addWidget(category2Label);
-
-    QHBoxLayout* layout = new QHBoxLayout();
-    dragMainLayout->addLayout(layout);
-
-    category1View = new QListView(this);
-    category1View->setDragEnabled(true);
-    category1View->setAcceptDrops(true);
-    category1View->setDropIndicatorShown(true);
-    category1View->setDefaultDropAction(Qt::MoveAction);
-    layout->addWidget(category1View);
-
-    category2View = new QListView(this);
-    category2View->setDragEnabled(true);
-    category2View->setAcceptDrops(true);
-    category2View->setDropIndicatorShown(true);
-    category2View->setDefaultDropAction(Qt::MoveAction);
-    layout->addWidget(category2View);
-
-    category1View->setModel(new QStringListModel());
-    category2View->setModel(new QStringListModel());
-
-    category1View->setStyleSheet
+    ui->cleanEnergyListView->setStyleSheet
             ("QListView { font-size: 20pt; font-weight: bold; }"
-             "QListView::item { background-color: #E74C3C; padding: 10%;"
-             "border: 1pt solid #C0392B; }"
-             "QListView::item::hover {background-color: #C0392B }");
-
-    category2View->setStyleSheet
-            ("QListView { font-size: 20pt; font-weight: bold; }"
-             "QListView::item { background-color: #2ECC71; padding: 10%;"
-             "border: 1pt solid #27AE60; }"
-             "QListView::item::hover {background-color: #27AE60 }");
+                        "QListView::item { background-color: #2ECC71; padding: 10%;"
+                        "border: 1pt solid #27AE60; }"
+                        "QListView::item::hover {background-color: #27AE60 }");
 
     QToolBar* toolBar = new QToolBar(this);
     addToolBar(toolBar);
@@ -79,6 +47,28 @@ MainWindow::MainWindow(QWidget *parent)
 
     toolBar->addAction(addAction);
     toolBar->addAction(removeAction);
+
+    ui->funProgressBar->setValue(0);
+    ui->funProgressBar->setMinimum(0);
+    ui->funProgressBar->setMaximum(100);
+    ui->funProgressBar->show();
+
+    ui->foodProgressBar->setValue(0);
+    ui->foodProgressBar->setMinimum(0);
+    ui->foodProgressBar->setMaximum(100);
+
+    ui->amenitiesProgressBar->setValue(0);
+    ui->amenitiesProgressBar->setMinimum(0);
+    ui->amenitiesProgressBar->setMaximum(100);
+
+    ui->energyProgressBar->setValue(0);
+    ui->energyProgressBar->setMinimum(0);
+    ui->energyProgressBar->setMaximum(100);
+
+    ui->environmentalImpactProgressBar->setValue(0);
+    ui->environmentalImpactProgressBar->setMinimum(0);
+    ui->environmentalImpactProgressBar->setMaximum(100);
+
 }
 
 MainWindow::~MainWindow()
@@ -89,15 +79,15 @@ MainWindow::~MainWindow()
 void MainWindow::onAdd()
 {
     //This is where instances of category sections are added to respective categories
-    category1View->model()->insertRow(category1View->model()->rowCount());
-    QModelIndex indexA = category1View->model()->
-            index(category1View->model()->rowCount() - 1, 0);
-    category1View->edit(indexA); //this is where 'edit: editing failed' is being thrown
+    ui->dirtyEnergyListView->model()->insertRow(ui->dirtyEnergyListView->model()->rowCount());
+    QModelIndex indexA = ui->dirtyEnergyListView->model()->
+            index(ui->dirtyEnergyListView->model()->rowCount() - 1, 0);
+    ui->dirtyEnergyListView->edit(indexA); //this is where 'edit: editing failed' is being thrown
 }
 
 void MainWindow::onRemove()
 {
-    QModelIndex indexR = category1View->currentIndex();
-    category1View->model()->removeRow(indexR.row());
+    QModelIndex indexR = ui->dirtyEnergyListView->currentIndex();
+    ui->dirtyEnergyListView->model()->removeRow(indexR.row());
 }
 
